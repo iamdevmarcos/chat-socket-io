68d2c7e028cbaaae5ecb904f6cc80bbe1c7af470
Marcos Andre <marcosdev.me@gmail.com>
2021-12-16 20:06:47 -0300

    adding socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 server.listen(process.env.PORT);
 
 app.use(express.static(path.join(__dirname, '../public')));
+
+io.on('connection', (socket) => {
+    console.log("connection detected...");
+});
