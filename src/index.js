1140aacd04d8706dd5411cfadf0cd6000e1f7638
Marcos Andre <marcosdev.me@gmail.com>
2021-12-16 14:20:18 -0300

    creating base of project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(4000);
+
+app.use(express.static(path.join(__dirname, '../public')));
