94953df454c26c5c99f9f2317af63b66639615eb
Marcos Andre <marcosdev.me@gmail.com>
2021-12-16 19:25:21 -0300

    adding dotenv

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
-server.listen(4000);
+server.listen(process.env.PORT);
 
 app.use(express.static(path.join(__dirname, '../public')));
