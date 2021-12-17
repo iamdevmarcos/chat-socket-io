b7ef2c67c8fa5df29768fecb64ea81190266a81c
Marcos Andre <marcosdev.me@gmail.com>
2021-12-16 21:23:41 -0300

    adding messages

--- public/main.js
+++ public/main.js
@@ -20,6 +20,18 @@ function renderUserList() {
     });
 }
 
+function addMessage(type, user, msg) {
+    let ul = document.querySelector('.chatList');
+    switch(type) {
+        case 'status':
+            ul.innerHTML += `<li class="m-status">${msg}</li>`;
+        break;
+        case 'msg':
+            ul.innerHTML += `<li class="m-txt"><span>${user} </span>${msg}</li>`;
+        break;
+    }
+}
+
 loginInput.addEventListener('keyup', (e) => {
     if(e.keyCode === 13) {
         let name = firstLetterUp(loginInput.value.trim());
@@ -37,11 +49,19 @@ socket.on('user-ok', (list) => {
     chatPage.style.display = 'flex';
     textInput.focus();
 
+    addMessage('status', null, 'Connected!');
+
     userList = list;
     renderUserList();
 })
 
 socket.on('list-update', (data) => {
+    if(data.joined) {
+        addMessage('status', null, `${data.joined} entered the chat.`);
+    }
+    if(data.left) {
+        addMessage('status', null, `${data.joined} left the chat.`);
+    }
     userList = data.list;
     renderUserList();
 });
